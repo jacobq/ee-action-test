ee8f6b6009087d1bda5ccc6c36cb3cf51469d1ad
Jacob Quant <jacobq@gmail.com>
2019-05-24 09:14:44 -0500

    ember install ember-electron

---
+++ ember-electron/electron-forge-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "make_targets": {
+    "win32": [
+      "squirrel"
+    ],
+    "darwin": [
+      "zip"
+    ],
+    "linux": [
+      "deb",
+      "rpm"
+    ]
+  },
+  "electronPackagerConfig": {
+    "packageManager": "npm"
+  },
+  "electronWinstallerConfig": {
+    "name": "ee_action_test"
+  },
+  "electronInstallerDebian": {},
+  "electronInstallerRedhat": {},
+  "github_repository": {
+    "owner": "",
+    "name": ""
+  },
+  "windowsStoreConfig": {
+    "packageName": "",
+    "name": "eeactiontest"
+  }
+};
